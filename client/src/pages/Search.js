0881c8a4411cff60c060fb54eb9c84b760b7e474
DoDatVu <datvu22@gmail.com>
2019-05-03 01:49:13 -0500

    pull data from api

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -3,6 +3,7 @@ import Wrapper from "../components/Wrapper/index";
 import SearchDiv from "../components/searchDiv";
 import Jumbotron from "../components/jumbotron";
 import BookDisplayDiv from "../components/bookDisplayDiv";
+import API from "../utils/API";
 
 const styles = {
   boop: {
@@ -13,17 +14,42 @@ const styles = {
 }
 
 class Search extends React.Component {
+  state = {
+    search: "",
+    results: []
+  }
 
+  searchBooks = query => {
+    API.search(query)
+      .then(res => this.setState({ results: res.data.items }))
+      .catch(err => console.log(err));
+  };
+
+  handleInputChange = event => {
+    const value = event.target.value;
+    this.setState({
+      search: value
+    });
+  };
+
+  handleFormSubmit = event => {
+    event.preventDefault();
+    this.searchBooks(this.state.search);
+  };
 
   render() {
 
     return (
       <Wrapper>
         <Jumbotron />
-        <SearchDiv />
+        <SearchDiv
+          search={this.state.search}
+          handleFormSubmit={this.handleFormSubmit}
+          handleInputChange={this.handleInputChange}
+        />
         <div style={styles.boop}>
           <h4>Results</h4>
-          <BookDisplayDiv />
+          <BookDisplayDiv results={this.state.results}/>
         </div>
       </Wrapper>
     )
